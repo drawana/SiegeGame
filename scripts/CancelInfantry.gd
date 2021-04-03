5585f54441e8cd8b98baae0d6e65f609f23625ff
Dallas <dallashblaney@gmail.com>
2021-04-03 18:16:11 -0400

    dfssdf

---
+++ scripts/CancelInfantry.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+func _ready():
+		
+	connect("pressed",self,"_on_Button_Pressed")
+
+func _on_Button_Pressed():
+	var QuickAttackInfantry = get_node("../QuickAttackInfantry")
+	QuickAttackInfantry.visible = false
+	
+	var DefendInfantry = get_node("../DefendInfantry")
+	DefendInfantry.visible = false
+	
+	var CancelInfantry = get_node("../CancelInfantry")
+	CancelInfantry.visible = false
