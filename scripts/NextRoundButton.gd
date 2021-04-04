92e449584c1f0cb70198489032e4e4c84cb62a18
Dallas <dallashblaney@gmail.com>
2021-04-04 02:51:19 -0400

    gdf

--- scripts/NextRoundButton.gd
+++ scripts/NextRoundButton.gd
@@ -40,7 +40,8 @@ func _on_Button_Pressed():
 		
 		var points = get_node("../PointsCounter")
 		var val = int(points.get_text()) 
-		points.set_text(str(val + 5))
+		var pointsToAdd = randi()%15 + 5
+		points.set_text(str(val + pointsToAdd))
 		
 		if (val + 5 >= 15):
 			var upgradeAvaliable = get_node("../UpgradeAvaliable2")
@@ -63,33 +64,41 @@ func _on_Button_Pressed():
 		greenAnimation.play("Test")
 		
 		var PointsCounterEnemy = get_node("../PointsCounterEnemy")
-		get_node("../PointsCounterEnemy").set_text(str(int(PointsCounterEnemy.get_text()) + 5))
 		
 		var multiplier = 1
+		var pointsToAdd2 = randi()%15 + 5
 		if difficulty == "Easy":
+			pointsToAdd2 = randi()%10 + 5
+			get_node("../PointsCounterEnemy").set_text(str(int(PointsCounterEnemy.get_text()) + pointsToAdd2))	
 			multiplier = 1
 		elif difficulty == "Medium":
+			pointsToAdd2 = randi()%15 + 10
+			get_node("../PointsCounterEnemy").set_text(str(int(PointsCounterEnemy.get_text()) + pointsToAdd2))
 			multiplier = 2
 		elif difficulty == "Hard":
+			pointsToAdd2 = randi()%20 + 10
+			get_node("../PointsCounterEnemy").set_text(str(int(PointsCounterEnemy.get_text()) + pointsToAdd2))
 			multiplier = 3
 			
 		var PointsCounterEnemyUpdated = get_node("../PointsCounterEnemy")
 		
-		#while PointsCounterEnemyUpdated > 15:
-		#	var random = randi()%3 + 1
+		while int(PointsCounterEnemyUpdated.get_text()) >= 15:
+			var random = randi()%3 + 1
 			
-		#	if random == 1:
-				
-				
-		#	elif random == 2:
-				
-				
-			#elif random == 3:
-			
-			
-			
-			
-		#if PointsCounterEnemyUpdated > 10:
+			if random == 1:
+				get_node("../InfantryLevelDisplayEnemy").set_text(str(int(get_node("../InfantryLevelDisplayEnemy").get_text()) + 1))
+				get_node("../PointsCounterEnemy").set_text(str(int(get_node("../PointsCounterEnemy").get_text()) - 10))
+			elif random == 2:
+				get_node("../NavalLevelDisplayEnemy").set_text(str(int(get_node("../NavalLevelDisplayEnemy").get_text()) + 1))
+				get_node("../PointsCounterEnemy").set_text(str(int(get_node("../PointsCounterEnemy").get_text()) - 10))
+			elif random == 3:
+				get_node("../ArtilleryLevelDisplayEnemy").set_text(str(int(get_node("../ArtilleryLevelDisplayEnemy").get_text()) + 1))
+				get_node("../PointsCounterEnemy").set_text(str(int(get_node("../PointsCounterEnemy").get_text()) - 10))
+		
+		var PointsCounterEnemyUpdated2 = get_node("../PointsCounterEnemy")
+		if int(PointsCounterEnemyUpdated2.get_text()) >= 10:
+			get_node("../InfantryLevelDisplayEnemy").set_text(str(int(get_node("../InfantryLevelDisplayEnemy").get_text()) + 1))
+			get_node("../PointsCounterEnemy").set_text(str(int(get_node("../PointsCounterEnemy").get_text()) - 10))
 			
 		
 			
