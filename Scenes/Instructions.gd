f968657d031d8b7df9ee307a03ca070560a2826a
drawana <drawana2@uwo.ca>
2021-04-05 01:04:35 -0400

    fixed instructions glitch
    fixed instructions glitch

--- Scenes/Instructions.gd
+++ Scenes/Instructions.gd
@@ -1,9 +1,4 @@
 extends MarginContainer
 
-const first_scene = preload("res://Scenes/Instructions2.tscn")
-
-
 func _on_Next_pressed():
-	get_parent().add_child(first_scene.instance())
-	queue_free()
-
+	get_tree().change_scene("res://Scenes/Instructions2.tscn")
