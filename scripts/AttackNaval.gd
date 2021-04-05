090d1981d4ef22c7a1034fd3789d8a5c7ef19a7f
Dallas <dallashblaney@gmail.com>
2021-04-05 16:03:32 -0400

    us naval and artillery animationds done

--- scripts/AttackNaval.gd
+++ scripts/AttackNaval.gd
@@ -10,14 +10,19 @@ func _on_Button_Pressed():
 	var points = get_node("../TheirHealthBar")
 	
 	var level = get_node("../NavalLevelDisplay").getVal()
-	var pointsToNotDecreaseBy = 0
-	if get_node("../NextRoundButton").getNavalDefendEnemy() == true:
-		var ourNavalLevel = int(get_node("../NavalLevelDisplayEnemy").get_text())
-		pointsToNotDecreaseBy = ((ourNavalLevel * 2) + randi()%5 + (1 * ourNavalLevel)) * 0.8
-	var pointsToDecreaseBy = (level * 4) + randi()%10 + (1 * level) - pointsToNotDecreaseBy
 	
-	if pointsToDecreaseBy > 0:
-		get_node("../TheirHealthBar").set_value(points.value - pointsToDecreaseBy)
+	if level == 1 or level == 2:
+		get_parent().get_parent().get_node("game2").get_node("group2 - fireball3").visible = true
+		get_parent().get_parent().get_node("game2").get_node("PPL LEVEL 4 - AnimationPlayer - Group2 - 5").play("SHIP LEVEL 1 + 2 - group 2 fireball")
+	if level == 3 or level == 4:
+		get_parent().get_parent().get_node("game2").get_node("group2 - fireball3").visible = true
+		get_parent().get_parent().get_node("game2").get_node("group2 - fireball2").visible = true
+		get_parent().get_parent().get_node("game2").get_node("PPL LEVEL 4 - AnimationPlayer - Group2 - 5").play("SHIP LEVEL 3 - group 2 fireball")
+	if level >= 5:
+		get_parent().get_parent().get_node("game2").get_node("group2 - fireball").visible = true
+		get_parent().get_parent().get_node("game2").get_node("group2 - fireball2").visible = true
+		get_parent().get_parent().get_node("game2").get_node("group2 - fireball3").visible = true
+		get_parent().get_parent().get_node("game2").get_node("PPL LEVEL 4 - AnimationPlayer - Group2 - 5").play("ship LEVEL 5 - group 2")
 	
 	greenAnimation.play("Test")
 	
@@ -31,4 +36,19 @@ func _on_Button_Pressed():
 	CancelNaval.visible = false
 	
 	get_node("../NextRoundButton").navalSet()
+	
+	yield(get_tree().create_timer(2.4), "timeout")
+	get_parent().get_parent().get_node("game2").get_node("group2 - fireball").visible = false
+	get_parent().get_parent().get_node("game2").get_node("group2 - fireball2").visible = false
+	get_parent().get_parent().get_node("game2").get_node("group2 - fireball3").visible = false
+	
+	var pointsToNotDecreaseBy = 0
+	if get_node("../NextRoundButton").getNavalDefendEnemy() == true:
+		var ourNavalLevel = int(get_node("../NavalLevelDisplayEnemy").get_text())
+		pointsToNotDecreaseBy = ((ourNavalLevel * 2) + randi()%5 + (1 * ourNavalLevel)) * 0.8
+	var pointsToDecreaseBy = (level * 4) + randi()%10 + (1 * level) - pointsToNotDecreaseBy
+	
+	if pointsToDecreaseBy > 0:
+		get_node("../TheirHealthBar").set_value(points.value - pointsToDecreaseBy)
+
 		
--- scripts/BigAttackArtillery.gd
+++ scripts/BigAttackArtillery.gd
@@ -7,17 +7,27 @@ func _ready():
 	connect("pressed",self,"_on_Button_Pressed")
 	
 func _on_Button_Pressed():
-	var points = get_node("../TheirHealthBar")
-	
 	var level = get_node("../ArtilleryLevelDisplay").getVal()
-	var pointsToNotDecreaseBy = 0
-	if get_node("../NextRoundButton").getArtilleryDefendEnemy() == true:
-		var ourArtilleryLevel = int(get_node("../ArtilleryLevelDisplayEnemy").get_text())
-		pointsToNotDecreaseBy = ((ourArtilleryLevel * 2) + randi()%5 + (1 * ourArtilleryLevel)) * 0.8
-	var pointsToDecreaseBy = (level * 3) + randi()%20 + (1 * level) - pointsToNotDecreaseBy
 	
-	if pointsToDecreaseBy > 0:
-		get_node("../TheirHealthBar").set_value(points.value - pointsToDecreaseBy)
+	if level == 1 or level == 2:
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").playing = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").get_node("group2 - fireball4").visible = true
+		get_parent().get_parent().get_node("game2").get_node("PPL LEVEL 4 - AnimationPlayer - Group2 - 5").play("LEVEL 1 - Cannon Group2")
+	if level == 3 or level == 4:
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").playing = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite2").playing = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").get_node("group2 - fireball4").visible = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite2").get_node("group2 - fireball5").visible = true
+		get_parent().get_parent().get_node("game2").get_node("PPL LEVEL 4 - AnimationPlayer - Group2 - 5").play("LEVEL 3 + 4 - Cannon Group 2")
+	if level >= 5:
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").playing = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite2").playing = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite3").playing = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").get_node("group2 - fireball4").visible = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite2").get_node("group2 - fireball5").visible = true
+		get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite3").get_node("group2 - fireball7").visible = true
+		get_parent().get_parent().get_node("game2").get_node("PPL LEVEL 4 - AnimationPlayer - Group2 - 5").play("LEVEL 5 - Cannon Group 2")
+	
 	greenAnimation.play("Test")
 	
 	var BigAttackArtillery = get_node("../BigAttackArtillery")
@@ -30,4 +40,25 @@ func _on_Button_Pressed():
 	CancelArtillery.visible = false
 	
 	get_node("../NextRoundButton").artillerySet()
+	
+	yield(get_tree().create_timer(.9), "timeout")
+	get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").playing = false
+	get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite2").playing = false
+	get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite3").playing = false
+	
+	yield(get_tree().create_timer(1.8), "timeout")
+	get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite1").get_node("group2 - fireball4").visible = false
+	get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite2").get_node("group2 - fireball5").visible = false
+	get_parent().get_parent().get_node("game2").get_node("CANNON GROUP 2 AnimatedSprite3").get_node("group2 - fireball7").visible = false
+	
+	var points = get_node("../TheirHealthBar")
+	var pointsToNotDecreaseBy = 0
+	if get_node("../NextRoundButton").getArtilleryDefendEnemy() == true:
+		var ourArtilleryLevel = int(get_node("../ArtilleryLevelDisplayEnemy").get_text())
+		pointsToNotDecreaseBy = ((ourArtilleryLevel * 2) + randi()%5 + (1 * ourArtilleryLevel)) * 0.8
+	var pointsToDecreaseBy = (level * 3) + randi()%20 + (1 * level) - pointsToNotDecreaseBy
+	
+	if pointsToDecreaseBy > 0:
+		get_node("../TheirHealthBar").set_value(points.value - pointsToDecreaseBy)
+
 		
